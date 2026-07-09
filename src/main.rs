dd90f840182fbd5ed7cabe48113250e135a98342
Ashan Niwantha <ashanniwantha.dev@gmail.com>
2026-07-09 09:45:31 +0530

    added file compression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use std::env::args;
+use std::fs::File;
+use std::io::{BufReader, copy};
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        return;
+    }
+
+    let f = File::open(args().nth(1).unwrap());
+    let mut input = BufReader::new(f.unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!(
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("Target len:{:?}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", start.elapsed());
 }
